3dda75feb92b60e50768e3e7f06b9f8a8c36fb80
chia <chiahui@senzing.com>
2020-03-16 10:56:51 -0400

    made output folder

--- main.tf
+++ main.tf
@@ -36,17 +36,7 @@ terraform {
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
-
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
-  description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table"
+module "webserver" {
+  source = "github.com/mahchiahui/terraform-aws-module//services/webserver?ref=v0.0.2"
+  server_name = "aws-webserver-2"
 }
---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
+
+output "webserver_ip" {
+  value       = module.webserver.webserver_ip
+  description = "The webserver ip"
+}
